f4f662d23a01a66862dbf3cc1964750825712e78
vishnurajendran <ssj5vishnu@gmail.com>
2018-02-09 18:22:08 +0530

    Project_T_Build_1

---
+++ Project_T/Scripts/Asteroid_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid_Script : MonoBehaviour {
+
+    public float Speed,MaxSpeed;
+    public GameObject Explosion;
+    public float inc;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+        transform.position += transform.forward * Speed * Time.deltaTime;
+
+	}
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision at " + transform.position);
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Project_T/Scripts/Key_move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_move : MonoBehaviour {
+    public float moveSpeed = 2f;
+    public float h, v;
+    public bool isPlayable = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isPlayable == true) {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            transform.position += transform.right * moveSpeed * h;
+            transform.position += transform.forward * moveSpeed * v;
+
+            Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+            pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
+            pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
+            transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+    }
+
+   public void setPlayability(bool val)
+    {
+        isPlayable = val;
+    }
+}
